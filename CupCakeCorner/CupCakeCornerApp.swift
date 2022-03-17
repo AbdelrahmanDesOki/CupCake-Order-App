3a8e3ea09a478b303e9d4152c94cfabdbe6a7e92
AbdelrahmanDesOki <desoki.abdelrahman@gmail.com>
2022-03-17 17:47:17 +0100

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Abdelrahman  Desoki on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
